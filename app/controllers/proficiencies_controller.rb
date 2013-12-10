c523e334e39d761c90f3ab70d634f19aad23d4f2
John Carpenter <jacarpe5@asu.edu>
2013-12-10 08:04:58 -0700

    added work, school. and proficiencies

--- app/controllers/proficiencies_controller.rb
+++ app/controllers/proficiencies_controller.rb
@@ -1,2 +1,15 @@
 class ProficienciesController < ApplicationController
+
+def new
+		@profile = Profile.find(params[:profile_id])
+		@proficiency = Proficiency.new
+	end
+	
+	def create
+		@profile = Profile.find(params[:profile_id])
+		@proficiency = @profile.proficiencies.create(params[:work].permit(:name, :title, :year))
+		@proficiency.save
+		redirect_to profile_path(@profile)
+	end
+	
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,16 @@
 class WorksController < ApplicationController
+
+	def new
+		@profile = Profile.find(params[:profile_id])
+		@work = Work.new
+	end
+	
+	def create
+		@profile = Profile.find(params[:profile_id])
+		@work = @profile.works.create(params[:work].permit(:name, :title, :year))
+		@work.save
+		redirect_to profile_path(@profile)
+	end
+	
+	
 end
